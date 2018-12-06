ed5cfc4e1e82e5c108a80f2bb90ae752b53b9c90
Rahul Koriya <30862800+rahulkoriya@users.noreply.github.com>
2018-12-06 12:13:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const mongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const db = require('./config/db');
